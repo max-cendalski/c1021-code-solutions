b209bb6f27d85d27111554d62ce6fb58bee7578c
Korneliu <olbiton@gmail.com>
2021-10-11 13:12:27 -0700

    completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 10;
+var height = 10;
+var area = width * height;
+console.log('value of area :', area);
+console.log('typeof area :', typeof area);
+
+var bill = 100;
+var payment = 50;
+var change = bill - payment;
+console.log('value of change :', change);
+console.log('typeof change :', typeof change);
+
+var quizzes = 10;
+var midterm = 10;
+var final = 3;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade :', grade);
+console.log('typeof grade :', typeof grade);
+
+var firstName = 'Max';
+var lastName = 'Cendalski';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName: ', fullName);
+console.log('typeof fullName :', typeof fullName);
+
+var ph = 12;
+var isAcidic = ph < 7;
+console.log('valueof isAcidic :', isAcidic);
+console.log('typeof isAcidic :', typeof isAcidic);
+
+var headCount = 100;
+var isSparta = headCount === 300;
+console.log('value of isSparta :', isSparta);
+console.log('typeof isSparta :', typeof isSparta);
+
+var motto = fullName;
+motto = fullName + ' ' + 'is the Goat';
+console.log('value of motto :', motto);
+console.log('typeof motto :', typeof motto);
