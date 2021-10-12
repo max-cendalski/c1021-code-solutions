53cd342bb2fd413c1c1d6d7d5284507d44ec5cfd
Korneliu <olbiton@gmail.com>
2021-10-12 12:37:36 -0700

    corrected buyAgain, added first item

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,15 +1,41 @@
 var orderHistory = [
   [
     {
+      orderNumber: 1143941689,
+      total: 34.00,
+      orderPlacedDate: 'August 4, 2020',
+      deliveryDate: 'Delivered Aug 8, 2020',
+      shipTo: 'JS Masher',
+      deliveredStatus: true,
+      buyAgain: '',
+      orderDetails: '',
+      invoice: '',
+      item: [
+        {
+          type: 'book',
+          price: 31.55,
+          invoice: '',
+          writeProductReview: '',
+          returnWindow: 'Sept 7, 2020',
+          additionalInfo: {
+            author: 'Rauschmayer, Dr. Axel',
+            title: 'JavaScript for impatient programmers',
+            productReview: '',
+            viewItem: ''
+          }
+        }
+      ]
+    },
+    {
       orderNumber: 11428755,
       total: 44.53,
       orderPlacedDate: 'July 19, 2020',
       deliveryDate: 'Delivered Jul 20 2020',
       shipTo: 'JS Masher',
       deliveredStatus: true,
-      returnWindow: 'Aug 19',
       buyAgain: '',
       orderDetails: '',
+      invoice: '',
       item: [
         {
           type: 'book',
@@ -35,16 +61,17 @@ var orderHistory = [
       deliveryDate: 'Delivered Jul 7, 2020',
       shipTo: 'JS Masher',
       deliveredStatus: true,
-      returnWindow: 'Aug 19, 2020',
+      buyAgain: '',
       orderDetails: '',
+      invoice: '',
       item: [
         {
-          type: 'computer parts',
+          type: 'computer hardware',
           price: 15.98,
           invoice: '',
-          returnWindow: 'Aug 19',
+          returnWindow: 'Aug 5',
           additionalInfo: {
-            name: 'gamecube controllelr',
+            name: 'gamecube controller',
             brand: 'Nintendo',
             productReview: '',
             buyAgain: '',
@@ -67,12 +94,12 @@ var orderHistory = [
       invoice: '',
       item: [
         {
-          type: 'computer parts',
+          type: 'computer hardware',
           price: 94.95,
           invoice: '',
           returnWindow: 'Aug 19, 2020',
           additionalInfo: {
-            name: 'gamecube controllelr',
+            name: 'gamecube controller',
             brand: 'Nintendo Switch',
             productReview: '',
             buyAgain: '',
