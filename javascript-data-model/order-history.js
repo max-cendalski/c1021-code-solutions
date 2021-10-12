e65183efac65189ad4c417a93d4d20b05afd152f
Korneliu <olbiton@gmail.com>
2021-10-12 11:25:10 -0700

    completed javascript-data-model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,3 +1,92 @@
 var orderHistory = [
-
+  [
+    {
+      orderNumber: 11428755,
+      total: 44.53,
+      orderPlacedDate: 'July 19, 2020',
+      deliveryDate: 'Delivered Jul 20 2020',
+      shipTo: 'JS Masher',
+      deliveredStatus: true,
+      returnWindow: 'Aug 19',
+      buyAgain: '',
+      orderDetails: '',
+      item: {
+        type: 'book',
+        price: 41.33,
+        invoice: '',
+        returnWindow: 'Sept7, 2020',
+        additionalInfo: {
+          author: 'Alexander, Christopher',
+          title: 'The timeless Way of Building',
+          productReview: '',
+          viewItem: ''
+        }
+      }
+    }
+  ],
+  [
+    {
+      orderNumber: 11428757,
+      total: 17.22,
+      orderPlacedDate: 'July 4, 2020',
+      deliveryDate: 'Delivered Jul 7, 2020',
+      shipTo: 'JS Masher',
+      deliveredStatus: true,
+      returnWindow: 'Aug 19, 2020',
+      orderDetails: '',
+      item: {
+        type: 'computer parts',
+        price: 15.98,
+        invoice: '',
+        returnWindow: 'Aug 19',
+        additionalInfo: {
+          name: 'gamecube controllelr',
+          brand: 'Nintendo',
+          productReview: '',
+          buyAgain: '',
+          viewItem: ''
+        }
+      }
+    }
+  ],
+  [
+    {
+      orderNumber: 1132883177,
+      total: 138.93,
+      orderPlacedDate: 'July 3, 2020',
+      deliveryDate: 'Delivered Jul 5, 2020',
+      shipTo: 'JS Masher',
+      deliveredStatus: true,
+      buyAgain: '',
+      orderDetails: '',
+      invoice: '',
+      item: [
+        {
+          type: 'computer parts',
+          price: 94.95,
+          invoice: '',
+          returnWindow: 'Aug 19, 2020',
+          additionalInfo: {
+            name: 'gamecube controllelr',
+            brand: 'Nintendo Switch',
+            productReview: '',
+            buyAgain: '',
+            viewItem: ''
+          }
+        }, {
+          type: 'book',
+          price: 15.98,
+          invoice: '',
+          returnWindow: 'Aug 19, 2020',
+          additionalInfo: {
+            author: 'Faroult, Stephane',
+            title: 'The Art of SQL',
+            productReview: '',
+            buyAgain: '',
+            viewItem: ''
+          }
+        }
+      ]
+    }
+  ]
 ];
