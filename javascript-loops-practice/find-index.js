d74d0e462ca1c25aaadc2346c5f36ad9293a89d2
Korneliu <olbiton@gmail.com>
2021-10-15 14:48:27 -0700

    oddOrEven not finished

--- javascript-loops-practice/find-index.js
+++ javascript-loops-practice/find-index.js
@@ -1,4 +1,12 @@
 /* exported findIndex */
-function findIndex(index, value) {
 
+function findIndex(array, value) {
+  var counter = 0;
+  for (var i = 0; i <= array.length; i++) {
+    counter++;
+    if (array[i] === value) {
+      return counter - 1;
+    }
+  }
+  return -1;
 }
