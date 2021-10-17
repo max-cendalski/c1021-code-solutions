fbae948028d7ce2febc3407f1ac9a2128c32e441
Korneliu <olbiton@gmail.com>
2021-10-17 16:17:07 -0700

    completed javascript-loops-practice

--- javascript-loops-practice/odd-or-even.js
+++ javascript-loops-practice/odd-or-even.js
@@ -1,1 +1,18 @@
 /* exported oddOrEven */
+// create newArray for words odd and even
+// create loop to iterate through an array of numbers
+// check if number is odd using if statement and  modulo
+// return newArray
+
+function oddOrEven(numbers) {
+  var newArray = [];
+  for (var i = 0; i <= numbers.length - 1; i++) {
+    if (numbers[i] % 2 === 0) {
+      newArray.push('even');
+    } else {
+      newArray.push('odd');
+    }
+
+  }
+  return newArray;
+}
