b164c1526d876693ae5730fd6950ebbc447eb624
Korneliu <olbiton@gmail.com>
2021-10-18 10:07:39 -0700

    started compact exercise

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,1 +1,11 @@
 /* exported compact */
+
+function compact(array) {
+  var newArray = [];
+  for (var i = 0; i <= array.length - 1; i++) {
+    if (array[i] === true) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
--- array-manipulation-1/src/reverse.js
+++ array-manipulation-1/src/reverse.js
@@ -1,1 +1,14 @@
 /* exported reverse */
+
+// create empty array
+// create loop to iterate through array from the last element to the first one
+// push elements to empty array
+// return newArray
+
+function reverse(array) {
+  var newArray = [];
+  for (var i = array.length - 1; i >= 0; i--) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
