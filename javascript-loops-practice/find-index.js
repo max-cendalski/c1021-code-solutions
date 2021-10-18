a31610015cb8b4c5a2315430a88d0042f07c65bc
Korneliu <olbiton@gmail.com>
2021-10-18 11:03:26 -0700

    removed counter and number variable from find-index and get-indexes

--- javascript-loops-practice/find-index.js
+++ javascript-loops-practice/find-index.js
@@ -1,11 +1,9 @@
 /* exported findIndex */
 
 function findIndex(array, value) {
-  var counter = 0;
   for (var i = 0; i <= array.length; i++) {
-    counter++;
     if (array[i] === value) {
-      return counter - 1;
+      return i;
     }
   }
   return -1;
--- javascript-loops-practice/get-indexes.js
+++ javascript-loops-practice/get-indexes.js
@@ -1,10 +1,9 @@
 /* exported getIndexes */
 function getIndexes(array) {
   var indArr = [];
-  var number = 0;
   for (var i = 0; i <= array.length - 1; i++) {
-    indArr.push(number);
-    number++;
+    indArr.push(i);
+
   }
   return indArr;
 }
