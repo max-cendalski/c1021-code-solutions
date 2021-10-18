9bdfe53dc3062f8c58f3f7a1bd7cbbcc1dd0ac9f
Korneliu <olbiton@gmail.com>
2021-10-18 10:53:54 -0700

    completed dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,16 @@
+console.log('hello, world');
+
+var h1Element = document.querySelector('h1');
+console.log(h1Element);
+
+var explanation = document.querySelector('#explanation');
+console.log(explanation);
+
+var hint = document.querySelector('.hint');
+console.log(hint);
+
+var nodeList = document.querySelectorAll('p');
+console.log(nodeList);
+
+var exampleLink = document.querySelectorAll('.example-link');
+console.log(exampleLink);
