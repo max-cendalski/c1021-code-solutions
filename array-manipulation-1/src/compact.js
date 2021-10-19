f7d2a10aa8b5b8d4eb4f824878385bf1f67d24a2
Korneliu <olbiton@gmail.com>
2021-10-18 21:59:25 -0700

    completed array-manipulation-1

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,9 +1,13 @@
 /* exported compact */
+// create empty array
+// create loop to iterate throgh elements
+// create if statement to check if element is truthy
+// push element to empty array and return array
 
 function compact(array) {
   var newArray = [];
   for (var i = 0; i <= array.length - 1; i++) {
-    if (array[i] === true) {
+    if (array[i]) {
       newArray.push(array[i]);
     }
   }
