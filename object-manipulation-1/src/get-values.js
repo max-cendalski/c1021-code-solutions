5fcae21eea8fc8c546df7561844004434f221b4e
Korneliu <olbiton@gmail.com>
2021-10-19 23:16:57 -0700

    completed getValues section

--- object-manipulation-1/src/get-values.js
+++ object-manipulation-1/src/get-values.js
@@ -1,1 +1,15 @@
 /* exported getValues */
+
+// create empty array
+// create for in loop to iterate through array
+// push value of key objects to new array
+// return newArray
+
+function getValues(object) {
+  var newArray = [];
+  for (var key in object) {
+    newArray.push(object[key]);
+  }
+
+  return newArray;
+}
