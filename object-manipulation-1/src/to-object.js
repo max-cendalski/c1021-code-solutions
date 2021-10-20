2717834a2676587e261357c9987e5ac4d5f4d526
Korneliu <olbiton@gmail.com>
2021-10-19 23:40:05 -0700

    completed object-manipulation-1

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -1,1 +1,11 @@
 /* exported toObject */
+
+// create empty object
+// use bracket notation to create object properties(key: value)
+// return newObj
+
+function toObject(keyValuePair) {
+  var newObj = {};
+  newObj[keyValuePair[0]] = keyValuePair[1];
+  return newObj;
+}
