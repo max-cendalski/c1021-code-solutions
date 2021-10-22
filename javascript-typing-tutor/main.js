6f1812ff86bb853e2d5fc767c47dbe4cd7258d67
Korneliu <olbiton@gmail.com>
2021-10-22 10:12:59 -0700

    nothing changed

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,21 +1,21 @@
 
-/*  var spanIndex = 0;
+/* var spanIndex = 0;
 var keyDownCount = 0;
 var numberOfMistakes = 0;
 var comments = ['what', 'whatever'];
 
 $spanKey = document.querySelectorAll('span');
-console.log($spanKey[1].innerText)
+console.log($spanKey[1].innerText);
 
 function handleTyping(event) {
   if (event.target === $spanKey[1]) {
     console.log('event.key', event.target);
-    console.log('bingo')
+    console.log('bingo');
 
   }
-  console.log('event.target', event.target)
+  console.log('event.target', event.target);
 }
 
 var $typingSection = document.querySelector('.typing-section');
-$typingSection.addEventListener('click', handleTyping);
+$typingSection.addEventListener('keydown', handleTyping);
  */
