4b8fa4d58450c2309d3f3800739484e3bcd6ae7a
Korneliu <olbiton@gmail.com>
2021-10-21 22:29:32 -0700

    started js section

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,15 @@
+
+/* var spanIndex = 0;
+var keyDownCount = 0;
+var numberOfMistakes = 0;
+var comments = ['what', 'whatever'];
+
+$spanKey = document.querySelectorAll('span');
+
+function handleTyping(event) {
+  console.log(event.target.value);
+}
+
+var $typingSection = document.querySelector('.typing-section');
+$typingSection.addEventListener('keydown', handleTyping);
+ */
