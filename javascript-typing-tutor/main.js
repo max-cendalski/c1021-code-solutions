938e19bc09c8326d5ae6efbadb8ab2af51ba21ae
Korneliu <olbiton@gmail.com>
2021-10-21 23:33:17 -0700

    stuck at keydown ;/

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,15 +1,21 @@
 
-/* var spanIndex = 0;
+/*  var spanIndex = 0;
 var keyDownCount = 0;
 var numberOfMistakes = 0;
 var comments = ['what', 'whatever'];
 
 $spanKey = document.querySelectorAll('span');
+console.log($spanKey[1].innerText)
 
 function handleTyping(event) {
-  console.log(event.target.value);
+  if (event.target === $spanKey[1]) {
+    console.log('event.key', event.target);
+    console.log('bingo')
+
+  }
+  console.log('event.target', event.target)
 }
 
 var $typingSection = document.querySelector('.typing-section');
-$typingSection.addEventListener('keydown', handleTyping);
+$typingSection.addEventListener('click', handleTyping);
  */
