f9a0c13159b613f07d2124d52d1f3acc0dc3fd63
Korneliu <olbiton@gmail.com>
2021-10-27 17:00:36 -0700

    started truncate

--- string-manipulation-2/src/truncate.js
+++ string-manipulation-2/src/truncate.js
@@ -1,1 +1,17 @@
 /* exported truncate */
+
+// declare 2 variables. One empty string ,second string with value = ...
+
+// use slice method to cut the string
+//
+
+/* var max = 'max cendalski';
+
+function truncate(length, string) {
+  var newString = '';
+  newString = string.slice(0, length);
+  return newString;
+}
+
+console.log(truncate(7, max));
+ */
