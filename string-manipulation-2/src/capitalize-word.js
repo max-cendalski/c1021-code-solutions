3d85a58033a92cb1b323a1b4dd69b1ead8689ce7
Korneliu <olbiton@gmail.com>
2021-10-28 23:28:11 -0700

    completed capitalizeWord

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,1 +1,24 @@
 /* exported capitalizeWord */
+
+// create variable for lowe rcase letters, upperCase letter ,result
+// use charAt() to capitalize 1 letter and store if variable
+// slice 1 letter
+// write conditional statemant
+
+function capitalizeWord(word) {
+  var lowerCaseString = '';
+  var upperCaseLetter = '';
+  var resultOfSlice = '';
+  var result = '';
+  lowerCaseString = word.toLowerCase();
+  upperCaseLetter = lowerCaseString.charAt(0).toUpperCase();
+  resultOfSlice = lowerCaseString.slice(1);
+  result = upperCaseLetter.concat(resultOfSlice);
+
+  if (lowerCaseString === 'javascript') {
+    return 'JavaScript';
+  } else {
+    return result;
+  }
+
+}
