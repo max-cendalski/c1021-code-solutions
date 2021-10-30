5037804f6b9f8eb26a843bed5081470706e23667
Korneliu <olbiton@gmail.com>
2021-10-30 15:47:25 -0700

    completed ransomCase

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,6 +1,20 @@
 /* exported ransomCase */
-/*
+// create variable for new string
+// create loop for each character
+// use modulo to check if character is even or odd
+// change letter to lowerCase or upperCase
+
 function ransomCase(string) {
+  var newString = '';
+
+  for (var i = 0; i < string.length; i++) {
+    if (i % 2 === 0) {
+      newString += string[i].toLowerCase();
+
+    } else {
+      newString += string[i].toUpperCase();
+    }
+  }
+  return newString;
 
 }
- */
