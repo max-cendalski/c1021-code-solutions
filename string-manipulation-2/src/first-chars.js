f5741c7a0365312880b6dc7fc9f27561c31c04ce
Korneliu <olbiton@gmail.com>
2021-10-30 17:44:30 -0700

    completed firstChar section

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,1 +1,8 @@
 /* exported firstChars */
+
+// using substring method to substract a new String
+
+function firstChars(length, string) {
+  var newString = string.substring(0, length);
+  return newString;
+}
