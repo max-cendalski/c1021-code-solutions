8a2f21bee22b499b7d80a995aa4d009071768921
Korneliu <olbiton@gmail.com>
2021-10-30 22:15:31 -0700

    completed string-manipulation-2

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,19 @@
 /* exported swapChars */
+
+// assing firstIndex and secondIndex to variables
+// convert string to array
+// replaced letters
+// convert array to string
+
+function swapChars(firstIndex, secondIndex, string) {
+  var result = '';
+
+  var firstLetter = string.charAt(firstIndex);
+  var secondLetter = string.charAt(secondIndex);
+
+  var newArray = string.split('');
+  newArray[firstIndex] = secondLetter;
+  newArray[secondIndex] = firstLetter;
+  result = newArray.join('');
+  return result;
+}
