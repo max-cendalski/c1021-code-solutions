53a7076f31d3fd3da19ee25d3a45cea5bc9d64b4
Korneliu <olbiton@gmail.com>
2021-11-01 15:45:08 -0700

    completed javascript-prototypes

--- javascript-prototypes/warner-bros.js
+++ javascript-prototypes/warner-bros.js
@@ -3,32 +3,32 @@
 var yakko = {
   age: 14,
   name: 'Yakko',
-  role: 'brother',
-  describe: function () {
-    var description = this.name + ' is a ' +
-                      this.age + '-year-old Warner ' + this.role + '.';
-    return description;
-  }
+  role: 'brother'
+
 };
 
 var wakko = {
   age: 11,
   name: 'Wakko',
-  role: 'brother',
-  describe: function () {
-    var description = this.name + ' is a ' +
-                      this.age + '-year-old Warner ' + this.role + '.';
-    return description;
-  }
+  role: 'brother'
+
 };
 
 var dot = {
   age: 10,
   name: 'Dot',
-  role: 'sister',
+  role: 'sister'
+
+};
+
+var warnerPrototype = {
   describe: function () {
     var description = this.name + ' is a ' +
-                      this.age + '-year-old Warner ' + this.role + '.';
+      this.age + '-year-old Warner ' + this.role + '.';
     return description;
   }
 };
+
+Object.setPrototypeOf(yakko, warnerPrototype);
+Object.setPrototypeOf(wakko, warnerPrototype);
+Object.setPrototypeOf(dot, warnerPrototype);
