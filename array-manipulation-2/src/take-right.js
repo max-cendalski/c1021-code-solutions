324263c18cf746aa13fd2e2bc299ca345d647287
Korneliu <olbiton@gmail.com>
2021-10-31 22:28:32 -0700

    completed takeRight section

--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,1 +1,19 @@
 /* exported takeRight */
+
+// create variable for new array
+// create loop starting from the end of array
+// add conditional statement for empy array
+
+// create new array
+// use slice method
+// return new array
+
+function takeRight(array, count) {
+  var newArray = [];
+  if (array === []) {
+    newArray = array;
+  } else {
+    newArray = array.slice(array.length - count);
+  }
+  return newArray;
+}
