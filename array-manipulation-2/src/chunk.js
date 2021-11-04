0e2a438d3b0d85b4b9284295f9db1dbd7d8d266a
Korneliu <olbiton@gmail.com>
2021-11-04 12:32:11 -0700

    working on chunk

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,5 +1,23 @@
 /* exported chunk */
-/* function chunk(array, size) {
+/*
+var array = ['one', 'two', 'three', 'four', 'five'];
 
+function chunk(array, size) {
+  var firstArray = [];
+  var secondArray = [];
+  var result = [];
+  for (var i = 0; i < size; i++) {
+    firstArray.push(array[i]);
+    // console.log(firstArray);
+  }
+  for (var j = array.length - size; j < array.length; j++) {
+    secondArray.push(array[j]);
+    // console.log(secondArray);
+  }
+  result.push(firstArray);
+  result.push(secondArray);
+  console.log(result);
+  return result;
 }
- */
+
+console.log(chunk(array, 3)); */
