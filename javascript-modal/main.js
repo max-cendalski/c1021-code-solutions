f451267a8cf8b0d516d1ccdfebe18c5581d18175
Korneliu <olbiton@gmail.com>
2021-11-03 21:30:40 -0700

    added handleClosingWindow function

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -2,11 +2,21 @@
 var $openModalWindow = document.querySelector('.open-modal-window');
 var $surveyWindow = document.querySelector('.survey-window-invisible');
 var $darkenedBackground = document.querySelector('.container');
+var $surveyWindowVisible = document.querySelector('.survey-window');
 
-$openModalWindow.addEventListener('click', handlingOpenModal);
 function handlingOpenModal(event) {
   event.preventDefault();
   $darkenedBackground.className = 'container-darkened';
   $surveyWindow.className = 'survey-window-visible';
   $openModalWindow.className = 'open-modal-window-darkened';
 }
+
+function handleClosingWindow(event) {
+  event.preventDefault();
+  $darkenedBackground.className = 'container';
+  $surveyWindow.className = 'survey-window-invisible';
+  $openModalWindow.className = 'open-modal-window';
+}
+
+$openModalWindow.addEventListener('click', handlingOpenModal);
+$surveyWindowVisible.addEventListener('click', handleClosingWindow);
