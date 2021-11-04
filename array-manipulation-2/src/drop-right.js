da3376b61418150cc4b8c09b634582d5aac9c903
Korneliu <olbiton@gmail.com>
2021-11-03 22:53:25 -0700

    completed dropRight section

--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,1 +1,14 @@
 /* exported dropRight */
+
+// create variable for new array
+// create loop ,numbers of loops = array.length-count
+// push items to the new array
+// return array
+
+function dropRight(array, count) {
+  var newArray = [];
+  for (var i = 0; i < array.length - count; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
