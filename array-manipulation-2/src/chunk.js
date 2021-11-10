eb37532074917be177ccbf4abae8d83221402cc9
Korneliu <olbiton@gmail.com>
2021-11-10 10:22:55 -0800

    trying two loops

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,23 +1,28 @@
 /* exported chunk */
+// create empty newArray 1
+// create empty array 2
+// create empty result
+// create for loop, iteration number = array.length
+// create another for loop to move items(size) to an array
+// add new array to results
+// empty newArray1
+//
 
-var array = ['one', 'two', 'three', 'four', 'five'];
+var array = ['one', 'two', 'three', 'four', 'five', 'six', 'seven'];
 
 function chunk(array, size) {
-  var arrayItems = [];
-  var arrayItems1 = [];
-  var firstArray = [];
-  var secondArray = [];
+  var newArray1 = [];
+  var newArray2 = [];
   var result = [];
-  for (var i = 0; i < size; i++) {
-    arrayItems.push(array[i]);
-    firstArray.push(arrayItems);
+  for (var i = 0; i < array.length; i = i + 2) {
+    for (var j = 0; j < size; j++) {
+      newArray1.push(array[j]);
+    }
+    result.push(newArray1);
+    console.log('first log of result:', result);
+    console.log('newArray1 after first inner loop: ', newArray1);
+    newArray1 = [];
   }
-  /*   for (var j = size; j < array.length; j++) {
-    arrayItems1.push(array[j]);
-    secondArray.push(arrayItems1);
-    arrayItems1 = [];
-  } */
-  console.log(firstArray);
 
   return result;
 }
