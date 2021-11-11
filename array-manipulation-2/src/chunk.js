09e89c887fd2dc3468112cc7fa148d764c9cf548
Korneliu <olbiton@gmail.com>
2021-11-11 11:56:32 -0800

    finished chunk section

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,30 +1,29 @@
 /* exported chunk */
-// create empty newArray 1
-// create empty array 2
-// create empty result
-// create for loop, iteration number = array.length
-// create another for loop to move items(size) to an array
-// add new array to results
+// create empty array -newArray
+// create empty array result
+// create variable = 0(to count iteration)
+// create for loop, running on entire array, starting from 0, counter + size
+// create another for loop to move items(size) to an newArray1
+// create if statement (loop runs no more times than array length)
+// add items to newArray
+// add newarray to results
 // empty newArray1
-//
-
-var array = ['one', 'two', 'three', 'four', 'five', 'six', 'seven'];
 
 function chunk(array, size) {
-  var newArray1 = [];
-  var newArray2 = [];
   var result = [];
-  for (var i = 0; i < array.length; i = i + 2) {
-    for (var j = 0; j < size; j++) {
-      newArray1.push(array[j]);
+  var newArray = [];
+  var count = 0;
+  for (var i = 0; i < array.length; i += size) {
+    for (var j = i; j < size + i; j++) {
+      count++;
+      if (count <= array.length) {
+        newArray.push(array[j]);
+      } else {
+        break;
+      }
     }
-    result.push(newArray1);
-    console.log('first log of result:', result);
-    console.log('newArray1 after first inner loop: ', newArray1);
-    newArray1 = [];
+    result.push(newArray);
+    newArray = [];
   }
-
   return result;
 }
-
-console.log(chunk(array, 2));
