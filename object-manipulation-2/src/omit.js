41144f7468f622a8efcb4e9c3c4001f6512f6723
Korneliu <olbiton@gmail.com>
2021-11-13 12:46:50 -0800

    started omit function

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,23 @@
 /* exported omit */
+/*
+var source = { foo: 1, bar: 2, baz: 3 };
+
+var keys = ['foo', 'baz'];
+
+function omit(source, keys) {
+  var newObject = {};
+  debugger;
+  for (var property in source) {
+    for (var i = 0; i < keys.length; i++) {
+      if (property !== keys[i]) {
+        console.log('property: ', property);
+        console.log('keys at i: ', keys[i]);
+        newObject[property] = source[property];
+      }
+    }
+  }
+  return newObject;
+}
+
+console.log(omit(source, keys));
+ */
