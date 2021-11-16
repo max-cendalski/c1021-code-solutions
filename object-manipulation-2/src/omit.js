ceab60ff58ce994a74d886e9bd85a5af29fc36ba
Korneliu <olbiton@gmail.com>
2021-11-15 16:05:33 -0800

    added break; to the loop, still not working

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,23 +1,27 @@
 /* exported omit */
-/*
+
 var source = { foo: 1, bar: 2, baz: 3 };
 
 var keys = ['foo', 'baz'];
 
 function omit(source, keys) {
   var newObject = {};
-  debugger;
+  // debugger;
   for (var property in source) {
+    debugger;
     for (var i = 0; i < keys.length; i++) {
-      if (property !== keys[i]) {
-        console.log('property: ', property);
-        console.log('keys at i: ', keys[i]);
+      // console.log(property);
+      console.log(keys[i]);
+
+      if (property === keys[i]) {
+        break;
+      } if (property !== keys[i]) {
         newObject[property] = source[property];
       }
     }
   }
+
   return newObject;
 }
 
 console.log(omit(source, keys));
- */
