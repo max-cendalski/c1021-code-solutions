0ae610e71d28a7d8f72555e6d150f93158ba802e
Korneliu <olbiton@gmail.com>
2021-11-19 23:36:05 -0800

    completed invert section

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,20 @@
 /* exported invert */
+
+// get value Object.values()
+// create variable vor values
+// create variable for counter to iterate over array
+// create for in loop to iterate over object
+// each iteration assing new key/value pair
+
+function invert(source) {
+  var newArray = Object.values(source);
+  var newObject = {};
+  var counter = 0;
+  for (var property in source) {
+    newObject[newArray[counter]] = property;
+    counter++;
+  }
+
+  return newObject;
+
+}
