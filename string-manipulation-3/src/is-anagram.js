f0f503e3df6cd74c2b958aede5c8b6c49c8b056e
Korneliu <olbiton@gmail.com>
2021-11-28 18:39:29 -0800

    completed isAnagram function

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,46 +1,23 @@
 /* exported isAnagram */
 
-// compare if strings length are the same
-
-var firstString = 'roses';
-var secondString = 'horse';
+// use replace method to remove space
+// compare length if different return false
+// use split method to convert to array, sort and convert to string
+// compare strings
 
 function isAnagram(firstString, secondString) {
-
-  var firstStringWithoutSpace = firstString.replace(/\s/g, '');
-  var secondStringWithoutSpace = secondString.replace(/\s/g, '');
-  debugger;
-
-  if (firstStringWithoutSpace.length !== secondStringWithoutSpace.length) {
+  var stringFirst = firstString.replaceAll(' ', '');
+  var stringSecond = secondString.replaceAll(' ', '');
+  if (stringFirst.length !== stringSecond.length) {
     return false;
   } else {
-    var result = '';
-    for (var i = 0; i < firstStringWithoutSpace.length; i++) {
-
-      for (var j = 0; j < secondStringWithoutSpace.length; j++) {
-        if (firstStringWithoutSpace[i] === secondStringWithoutSpace[j]) {
-          result += firstStringWithoutSpace[i];
-          break;
-        }
-      }
-    }
-    if (result === firstStringWithoutSpace) {
+    var stringFirstSplit = stringFirst.split('').sort().join('');
+    var stringSecondSplit = stringSecond.split('').sort().join('');
+    if (stringFirstSplit === stringSecondSplit) {
       return true;
     } else {
       return false;
     }
-
   }
+
 }
-
-/*  var firstArray = firstString.split().sort().join();
-  var secondArray = secondString.split().sort().join();
-  console.log(secondArray);
-  if (firstArray === secondArray) {
-    return true;
-  } else {
-    return false;
-  }
-} */
-
-console.log(isAnagram(firstString, secondString));
