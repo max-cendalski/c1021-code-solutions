60728aa1c2dd2857fea8d7e0fc0caba01ccb7a6e
Korneliu <olbiton@gmail.com>
2021-11-29 16:39:51 -0800

    completed node-notes-cli

--- node-notes-cli/main.js
+++ node-notes-cli/main.js
@@ -1,30 +1,29 @@
 var fs = require('fs');
 
 fs.readFile('data.json', 'utf8', (err, data) => {
+
+  function write(data) {
+    var dataStringify = JSON.stringify(thingsToDo, null, 2);
+    fs.writeFile('data.json', dataStringify, err => {
+      if (err) throw err;
+    });
+  }
+
   if (err) throw err;
   var thingsToDo = JSON.parse(data);
   if (process.argv[2] === 'read') {
     for (var key in thingsToDo.notes) {
       console.log(key + '.' + thingsToDo.notes[key]);
     }
-    console.log(thingsToDo.nextId);
   } if (process.argv[2] === 'create') {
     thingsToDo.notes[thingsToDo.nextId] = process.argv[3];
     thingsToDo.nextId++;
-    var dataStringify = JSON.stringify(thingsToDo, null, 2);
-    fs.writeFile('data.json', dataStringify, err => {
-      if (err) throw err;
-    });
+    write(thingsToDo);
+  } if (process.argv[2] === 'delete') {
+    delete (thingsToDo.notes[process.argv[3]]);
+    write(thingsToDo);
+  } if (process.argv[2] === 'update') {
+    thingsToDo.notes[process.argv[3]] = process.argv[4];
+    write(thingsToDo);
   }
 });
-
-/*
-{
-  "nextId": 5,
-  "notes": {
-    "1": "The event loop is how a JavaScript runtime pushes asynchronous callbacks onto the stack once the stack is cleared.",
-    "2": "Prototypal inheritance is how JavaScript objects delegate behavior.",
-    "3": "In JavaScript, the value of `this` is determined when a function is called; not when it is defined.",
-    "4": "A closure is formed when a function retains access to variables in its lexical scope."
-  }
-} */
