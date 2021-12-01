478d35372fd49cb7b9fe5139340a5e96756a74d0
Korneliu <olbiton@gmail.com>
2021-12-01 15:53:59 -0800

    completed express-post-json

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+express.json();
+app.use(express.json());
 
 var nextId = 1;
 var grades = {};
@@ -9,7 +11,17 @@ app.get('/api/grades', (req, res) => {
   for (var key in grades) {
     gradesArray.push(grades[key]);
   }
-  res.send(gradesArray);
+  res.json(gradesArray);
+});
+
+app.post('/api/grades', (req, res) => {
+  res.setHeader('content-type', 'application/json', 'charset Utf-8');
+  var gradeObject = req.body;
+  gradeObject.id = nextId;
+  nextId++;
+  grades[nextId] = gradeObject;
+  res.json(grades);
+  res.status(201);
 });
 
 app.listen(3000, () => {
