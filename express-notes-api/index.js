220008621be1f102ce613d72653bb72a688510e3
Korneliu <olbiton@gmail.com>
2021-12-01 18:01:56 -0800

    completed partially get by id

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -3,7 +3,25 @@ var app = express();
 express.json();
 app.use(express.json());
 
-var notes = {};
+var notes = {
+  1: {
+    content: 'The event loop is how a JavaScript runtime pushes asynchronous callbacks onto the stack once the stack is cleared.',
+    id: 1
+  },
+  2: {
+    content: 'Prototypal inheritance is how JavaScript objects delegate behavior.',
+    id: 2
+  },
+  3: {
+    content: 'In JavaScript, the value of `this` is determined when a function is called; not when it is defined.',
+    id: 3
+  },
+  4: {
+    content: 'A closure is formed when a function retains access to variables in its lexical scope.',
+    id: 4
+  }
+};
+var nextId = 1;
 
 app.get('/api/notes', (req, res) => {
   var notesArray = [];
@@ -11,6 +29,16 @@ app.get('/api/notes', (req, res) => {
     notesArray.push(notes[key]);
   }
   res.json(notesArray);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  var id = parseInt(req.params.id);
+  if (id !== Number.isInteger(id)) {
+    console.log('typeof:', typeof id);
+    res.sendStatus(400);
+  } else {
+    res.json(notes[id]);
+  }
 });
 
 app.listen(3000, () => {
