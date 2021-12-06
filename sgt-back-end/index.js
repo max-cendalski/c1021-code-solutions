69773a36e7d3a615532186edf5774e949ecefd69
Korneliu <olbiton@gmail.com>
2021-12-06 15:43:03 -0800

    inital commit

---
+++ sgt-back-end/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const pg = require('pg');
+const app = express();
+
+const db = new pg.Pool({
+  connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
