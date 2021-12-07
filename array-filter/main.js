f144803efbe6e77050f1e19a56b9a8d1e57eee2f
Korneliu <olbiton@gmail.com>
2021-12-07 15:09:05 -0800

    completed array-filter

--- array-filter/main.js
+++ array-filter/main.js
@@ -11,3 +11,16 @@ const names = [
   'Janese',
   'Donna'
 ];
+
+var evenNumbers = numbers.filter(number => number % 2 === 0);
+
+var overFive = numbers.filter(number => number > 5);
+
+var startWithE = names.filter(word => word.startsWith('E'));
+
+var haveD = names.filter(word => word.toLowerCase().includes('d'));
+
+console.log(evenNumbers);
+console.log(overFive);
+console.log(startWithE);
+console.log(haveD);
