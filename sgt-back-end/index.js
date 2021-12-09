9c65cf5040a25c66ae0e981735cb62f0048cef8b
Korneliu <olbiton@gmail.com>
2021-12-08 23:12:18 -0800

    completed POST section

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const pg = require('pg');
 const app = express();
+app.use(express.json());
 
 const db = new pg.Pool({
   connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
@@ -25,18 +26,112 @@ app.get('/api/grades', (req, res) => {
 });
 
 app.post('/api/grades', (req, res) => {
-  console.log('req.params', req.params);
   const text = 'INSERT INTO grades(name, course, score) VALUES($1,$2,$3) RETURNING *';
-  const values = ['req.params.name', 'req.params.course', 'req.params.course'];
+  const values = [req.body.name, req.body.course, req.body.score];
 
-  db.query(text, values)
-    .then(result => {
-      console.log(result.rows[0]);
-    })
-    .catch(err => console.error(err.stack));
+  if (!values) {
+    res.status(400).json({ error: 'missing data' });
+  } else {
+    db.query(text, values)
+      .then(result => {
+        res.status(200).json(result.row);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: 'unexpected error occured' });
+      });
+  }
 });
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('server is up on port 3000');
 });
+
+/* var express = require('express');
+var fs = require('fs');
+var app = express();
+express.json();
+app.use(express.json());
+var notesObject = require('./data.json');
+
+app.get('/api/notes', (req, res) => {
+  var notesArray = [];
+  for (var key in notesObject.notes) {
+    notesArray.push(notesObject.notes[key]);
+  }
+  res.json(notesArray);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  var id = parseInt(req.params.id);
+  if (!Number.isInteger(id) || id <= 0) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (!notesObject.notes[id]) {
+    res.status(404).json({ error: "can't find note with id " + id });
+  } else {
+    res.json(notesObject.notes[id]);
+  }
+});
+
+app.post('/api/notes', (req, res) => {
+  var newNoteObject = req.body;
+  if (!newNoteObject.content) {
+    res.status(400).json({ error: 'content is a required field' });
+  } else {
+    var count = notesObject.nextId;
+    notesObject.notes[count] = newNoteObject;
+    newNoteObject.id = count;
+    notesObject.notes[count] = newNoteObject;
+    notesObject.nextId++;
+    var objectToSave = JSON.stringify(notesObject);
+    fs.writeFile('./data.json', objectToSave, err => {
+      if (err) {
+        res.status(500).json({ error: 'An unexpected error occuried' });
+      } else {
+        res.status(201).json(newNoteObject);
+      }
+    });
+  }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  var id = parseInt(req.params.id);
+  if (!Number.isInteger(id) || id <= 0) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (!notesObject.notes[id]) {
+    res.status(404).json({ error: "can't find note with id " + id });
+  } else {
+    delete notesObject.notes[id];
+    var objectToSave = JSON.stringify(notesObject);
+    fs.writeFile('./data.json', objectToSave, err => {
+      if (err) {
+        res.status(500).json({ error: 'An unexpected error occuried' });
+      } else {
+        res.status(204).json({ notesObject });
+      }
+    });
+  }
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  var id = parseInt(req.params.id);
+  if (!Number.isInteger(id) || !req.body) {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  } else if (!notesObject.notes[id]) {
+    res.status(404).json({ error: 'No matching note' });
+  } else {
+    notesObject.notes[id].content = req.body.content;
+    var updatedObject = notesObject.notes[id];
+    var objectToSave = JSON.stringify(notesObject);
+    fs.writeFile('./data.json', objectToSave, err => {
+      if (err) {
+        res.status(500).json({ error: 'An unexpected error occuried' });
+      } else {
+        res.status(200).json(updatedObject);
+      }
+    });
+  }
+});
+
+ */
