31eb8398e5ea5bb6ce7d17e4d26cd91bbc7b3350
Korneliu <olbiton@gmail.com>
2021-12-12 16:15:49 -0800

    added interval and icons

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -4,27 +4,37 @@ class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      timer: true
+      icon: '../src/icons/play-solid.svg',
+      time: 0
     };
+    this.handleTimer = this.handleTimer.bind(this);
+  }
+
+  handleTimer() {
+    setInterval(() => {
+      this.setState({
+        time: this.state.time + 1
+      });
+    }, 1000);
   }
 
   render() {
     return (
       <div>
         <div className="row">
-        <div className="circle-container">
-          <div className="circle">
-            <div className="stop-watch">
-              <h2>6</h2>
+          <div className="circle-container" onClick={this.handleTimer}>
+            <div className="circle">
+              <div className="stop-watch">
+                <h2>{this.state.time}</h2>
+              </div>
             </div>
           </div>
         </div>
-        <div className="icon">
-        </div>
+        <div className="row">
+          <img src={this.state.icon} />
         </div>
       </div>
     );
-
   }
 }
 
