9eaafa7e7cee6b9a9cea123106220b2aff5c7fe7
Korneliu <olbiton@gmail.com>
2021-12-13 21:59:35 -0800

    added handleClick to each li element

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -6,12 +6,31 @@ export default class AppDrawer extends React.Component {
     this.state = {
       darker: false
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      darker: !this.state.darker
+    });
   }
 
   render() {
     return (
-      <div>
-      <i className="fas fa-bars"></i>
+      <div className="container">
+        <div className="row">
+          {!this.state.darker
+            ? <i className="fas fa-bars" onClick={this.handleClick}></i>
+            : <ul>
+                <h4>Choose your game</h4>
+                <li onClick={this.handleClick}>The Legend Of Zelda</li>
+                <li onClick={this.handleClick}>A Link to the Past</li>
+                <li onClick={this.handleClick}>Ocarina of Time</li>
+                <li onClick={this.handleClick}>The Wind Waker</li>
+                <li onClick={this.handleClick}>Breath of the Wild</li>
+            </ul>
+          }
+        </div>
       </div>
     );
   }
