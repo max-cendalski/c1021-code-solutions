c8ab0858d49ae2bdef6f50e55cadd859d6f78c1c
Korneliu <olbiton@gmail.com>
2021-12-14 15:21:59 -0800

    added map

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 
-export default class Accordion extends Component {
-
+export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       visible: false
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log(event.target.value);
   }
 
   render() {
-    const newArray = [
-      { name: 'Hypertext Markup Language', description: 'The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser.' },
-      { name: 'Cascading Style Sheets', description: 'Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.' },
-      { name: 'JavaScript', description: 'JavaScript, often abbreviated JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.' }
-    ];
     return (
-      <ul>
-        {newArray.map(item => <li key={newArray.name}>item.name</li>)}
+      <ul onClick={this.handleClick}>
+        {this.props.topics.map(item => <li key={item.header}>{item.header}</li>)}
       </ul>
     );
   }
