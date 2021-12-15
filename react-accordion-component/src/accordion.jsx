109e09923df83f0aa477454ed6bf976c3e84af06
Korneliu <olbiton@gmail.com>
2021-12-14 21:08:45 -0800

    completed react-accordion-component

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -4,20 +4,51 @@ export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      visible: false
+      index: 0,
+      check: false,
+      detailsContainer: 'details-container'
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
-    console.log(event.target.value);
+    const topics = this.props.topics;
+    const element = event.target.textContent;
+    if (this.state.check === true) {
+      this.setState({
+        check: false
+      });
+    } else {
+      for (var i = 0; i < topics.length; i++) {
+        if (this.props.topics[i].header === element) {
+          this.setState({
+            index: i,
+            check: true
+          });
+        }
+      }
+    }
   }
 
   render() {
+    let detail = this.state.detailsContainer;
     return (
       <ul onClick={this.handleClick}>
-        {this.props.topics.map(item => <li key={item.header}>{item.header}</li>)}
-      </ul>
+        {this.props.topics.map((item, index) => {
+          if (index === this.state.index && this.state.check === true) {
+            detail = 'details-container-visible';
+          } else {
+            detail = 'details-container';
+          }
+          return (
+            <li key={index}>
+              <div className='header-container'>{item.header}</div>
+              <div className={detail}>{item.details}</div>
+            </li>
+          );
+        })
+        }
+     </ul>
     );
   }
 }
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Accordion from './accordion';
 
 ReactDOM.render(<Accordion topics = {[
-  { header: 'Hypertext Markup Language', description: 'The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser.' },
-  { header: 'Cascading Style Sheets', description: 'Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.' },
-  { header: 'JavaScript', description: 'JavaScript, often abbreviated JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.' }
+  { header: 'Hypertext Markup Language', details: 'The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser.' },
+  { header: 'Cascading Style Sheets', details: 'Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.' },
+  { header: 'JavaScript', details: 'JavaScript, often abbreviated JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.' }
 ]}/>, document.querySelector('#root'));
