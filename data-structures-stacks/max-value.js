e70056fb976bae725da91fcbf5fe69c44f76afb1
Korneliu <olbiton@gmail.com>
2022-01-11 17:00:50 -0800

    if statement in maxValue

--- data-structures-stacks/max-value.js
+++ data-structures-stacks/max-value.js
@@ -5,7 +5,7 @@ function maxValue(stack) {
   let number = 0;
   if (stack.peek() === undefined) {
     return -Infinity;
-  } if (stack.peek() !== undefined) {
+  } else {
     while (stack.peek() !== undefined) {
       number = stack.pop();
       numberArray.push(number);
